98980684214ec4fa947d96aa0460cc67c342db45
kito0 <drewshah0@gmail.com>
2022-01-15 16:20:33 -0700

    Add create, getAll contract methods

--- contracts/PostList.sol
+++ contracts/PostList.sol
@@ -1,15 +1,17 @@
-pragma solidity >=0.4.21 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
 
 contract PostList {
     uint public postCount = 0;
 
-    struct post {
+    struct Post {
         uint id;
         string author;
         string body;
     }
 
-    mapping(uint => post) public posts;
+    mapping(uint => Post) public posts;
+    // Post[] posts;
 
     // constructor() public {
     //     createPost("Tester", "Test post");
@@ -17,6 +19,18 @@ contract PostList {
 
     function createPost(string memory _author, string memory _body) public {
         postCount ++;
-        posts[postCount] = post(postCount, _author, _body);
+        posts[postCount] = Post(postCount, _author, _body);
+    }
+
+    function getPostCount() public view returns (uint) {
+        return postCount;
+    }
+
+    function getPosts() public view returns (Post[] memory) {
+        Post[] memory list = new Post[](postCount);
+        for(uint i = 0; i < postCount; i++) {
+            list[i] = posts[i];
+        }
+        return list;
     }
 }
